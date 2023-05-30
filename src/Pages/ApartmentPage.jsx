2ea6678f0869bb9169598e8bcc9ada61ea14065b
Benjamin Hautefaye <titouchef@hotmail.fr>
2023-05-30 11:42:43 +0200

    Maj apartment

--- src/Pages/ApartmentPage.jsx
+++ src/Pages/ApartmentPage.jsx
@@ -7,22 +7,31 @@ function ApartmentPage() {
         <div>
             <img src="https://picsum.photos/800/400" alt="" />
         </div>
-            <div className='apartment-page_subtitle'>
+            <div className='apartment_header'>
+            <div className='apartment_title'>
         <h1>Crazy loft on the Canal Saint-Martin</h1>
         <h2>Paris, Ile de france </h2>
-                <span className="apartment_tag">Cozy</span>
-                <span className="apartment_tag">Canal</span>
-                <span className="apartment_tag">Paris 10</span>
+        <div className='apartment_tags'>
+                <span >Cozy</span>
+                <span >Canal</span>
+                <span >Paris 10</span>
+                </div>
                 </div>
                 <div className="apartment_owner">
-                    <h3>Alexandre Dumas</h3>
+                    <div className='apartment_owner_details'>
+                    <h3> 
+                    <span> Alexandre</span> 
+                    <span>Dumas</span>
+                    </h3>
                     <div className="apartment_owner_badge"></div>
+                    </div>
                     <div className='apartment_owner_stars'>
-                    <span>☆</span>
-                    <span>☆</span>
-                    <span>☆</span>
-                    <span>☆</span>
-                    <span>☆</span>
+                    <span className='on'>★</span>
+                    <span className='on'>★</span>
+                    <span className='on'>★</span>
+                    <span className='off'>★</span>
+                    <span className='off'>★</span>
+                    </div>
                     </div>
                     </div>
                     <div className="apartment_description">
