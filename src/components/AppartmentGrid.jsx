065dc23117420967075275b3ab02a235ca51447a
Benjamin Hautefaye <titouchef@hotmail.fr>
2023-06-05 17:50:42 +0200

    Ajustements finaux

--- src/components/AppartmentGrid.jsx
+++ src/components/AppartmentGrid.jsx
@@ -1,28 +1,30 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import "./AppartmentGrid.scss";
 import ApartmentCard from "./ApartmentCard.jsx";
-
 
 function AppartmentGrid() {
   const [apartments, setApartments] = useState([]);
   
-  useEffect(fetchApartments, []);
-  //useEffect avec une array vide == execute cette fonction au chargement du composant 
+  useEffect(() => {
+    fetchApartments();
+  }, []);
   
   function fetchApartments() {
-    fetch ('db.json')
-    .then((res) => res.json())
-    .then((res) => setApartments(res))
-    .catch(console.error);
-    
+    fetch('db.json')
+      .then((res) => res.json())
+      .then((res) => setApartments(res))
+      .catch(console.error);
   }
+  
   return (
     <div className='grid'> 
-    {apartments.map((apartment) => (
-    <ApartmentCard title={apartment.title} imageUrl={apartment.cover} id={apartment.id} />
-    ))}
+      {apartments.map((apartment) => (
+        <div className="card-wrapper" key={apartment.id}>
+          <ApartmentCard title={apartment.title} imageUrl={apartment.cover} id={apartment.id} />
+        </div>
+      ))}
     </div>
   );
 }
 
-export default AppartmentGrid
+export default AppartmentGrid;
